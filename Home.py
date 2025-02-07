4be476b43a421dde1b5dff992becc6d83ae313b7
shuvankr7 <92281019+shuvankr7@users.noreply.github.com>
2025-02-07 19:41:10 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,8 +8,8 @@ st.set_page_config(
 st.header("Habibi Welcome to Gurgaon! 👋")
 
 st.write(''' Here we have 3 section\n
-             1. Price Predictor - Here user can input 12 parameters and according to that parameter predicted flat price range will be shown
-             2. Analysis App - Here you will be visualizing different analysis
+             1. Price Predictor - Here user can input 12 parameters and according to that parameter predicted flat price range will be shown\n
+             2. Analysis App - Here you will be visualizing different analysis\n
              3. Recommend Appartments - Model will recommend you top 5 appartments based on your inputs''')
 
 st.sidebar.success("Select a demo above.")
