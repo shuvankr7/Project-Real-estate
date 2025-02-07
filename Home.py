becc685612bcf06d8e44077e774e1f547f3040fb
shuvankr7 <92281019+shuvankr7@users.noreply.github.com>
2025-02-07 19:53:16 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,9 +13,28 @@ st.write('Please note - This model is build on data of gurgaon only')
 
 
 st.markdown("""
----
-**© 2024 Shuvankar Naskar** | Built with ❤️ using Streamlit  
-🔗 [LinkedIn](www.linkedin.com/in/shuvankar-naskar-data-scientist)
-""")
+    <style>
+        .footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            text-align: center;
+            padding: 10px;
+            background-color: #f1f1f1;
+            color: black;
+            font-size: 14px;
+        }
+    </style>
+    <div class="footer">
+        <p>© 2025 Shuvankar Naskar | Powered by Streamlit 🚀</p>
+        <p>
+            <a href="www.linkedin.com/in/shuvankar-naskar-data-scientist" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="20"/>
+                LinkedIn
+            </a>
+        </p>
+    </div>
+""", unsafe_allow_html=True)
+
 
 st.sidebar.success("Select a demo above.")
