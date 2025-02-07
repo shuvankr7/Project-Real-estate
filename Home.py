4db6bcb42556eb8815f266cc97cd4bb3f670a405
shuvankr7 <92281019+shuvankr7@users.noreply.github.com>
2025-02-07 19:48:05 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,5 +10,6 @@ st.header("Habibi Welcome to Gurgaon! 👋")
 st.write(''' Here we have 3 section - \n1. Price Predictor - Here user can input 12 parameters and according to that parameter predicted flat price range will be shown\n2. Analysis App - Here you will be visualizing different analysis\n3. Recommend Appartments - Model will recommend you top 5 appartments based on your inputs''')
 st.write('\n')
 st.write('Please note - This model is build on data of gurgaon only')
+st.footer("created by - www.linkedin.com/in/shuvankar-naskar-data-scientist")
 
 st.sidebar.success("Select a demo above.")
